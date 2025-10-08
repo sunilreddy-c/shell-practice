e29f94112c74d578132922225e1848733a3cd1e1
sunilreddyc <sunilreddyc006@gmail.com>
2025-10-08 16:12:17 +0530

    added

---
+++ shell-practice/03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+person1=trunp
+person2=musk
+
+echo "$person1 : hey musk how re you"
+echo "$person2: iam fine "
+echo "$person1 : how was the business"
+echo "$person2 : going good and not bad"
+echo "$person1 : where is our flight now"
+echo "$person2 : its in outside"
