e2dd158c555a66eb7908f93b1ff18c3db4e4253e
sunilreddyc <sunilreddyc006@gmail.com>
2025-10-08 17:10:29 +0530

    added

--- shell-practice/05-array.sh
+++ shell-practice/05-array.sh
@@ -3,4 +3,7 @@
 MOVIES=("sunil" "anil" "reddy" "ashok")
 
 echo "first movie" ${MOVIES[0]}
-
+echo "first movie" ${MOVIES[0]}
+echo "second movie" ${MOVIES[1]}
+echo "third movie" ${MOVIES[2]}
+ echo "all movies" ${MOVIES[@]}
