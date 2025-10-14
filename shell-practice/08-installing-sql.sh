f00d7a048c1a7e3b8d28d8d3aca780f931d03104
sunilreddyc <sunilreddyc006@gmail.com>
2025-10-14 14:18:47 +0530

    added

---
+++ shell-practice/08-installing-sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: please run the script with root access"
+    exit1
+else 
+then 
+   echo "you are running with root access"
+fi       
