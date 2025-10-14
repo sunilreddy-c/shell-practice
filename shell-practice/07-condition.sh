5bb96da7363c5edee8cc37fcb3837709fdb306d4
sunilreddyc <sunilreddyc006@gmail.com>
2025-10-14 08:24:54 +0530

    added

---
+++ shell-practice/07-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#-gt --> greater than
+#-lt --> less than
+#-eq --> equal to
+#-eq --> not equal to
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10]
+then
+echo "given number is $NUMBER is lessthan 10"
+else
+echo "given number is not $NUMBER is not lessthan 10"
+
