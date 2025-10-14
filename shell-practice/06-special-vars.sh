5e35a119720146585a8fb0e2b79195c074518653
sunilreddyc <sunilreddyc006@gmail.com>
2025-10-14 08:12:48 +0530

    added

--- shell-practice/06-special-vars.sh
+++ shell-practice/06-special-vars.sh
@@ -7,6 +7,6 @@ echo "current directory:$PWD"
 echo "user running this script:$USER"
 echo "home directory of user:$HOME"
 echo "PID of the script:$$"
-slepp 10 &
+sleep 10 &
 echo "PID of last command in background:$!"
 
